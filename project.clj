7c7d347bd3b66444baa0dd221efccd708a0494a0
Gary Fredericks <fredericksgary@gmail.com>
2014-04-30 21:37:37 -0500

    Set group name and clojure version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject muxrepl "0.1.0-SNAPSHOT"
+(defproject com.gfredericks/muxrepl "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]])
